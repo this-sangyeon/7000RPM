db1616fff47e23f8ccf0ee3320d6aa4f73cec590
이상연 <54699646+tkddus001@users.noreply.github.com>
2021-10-12 19:04:26 +0900

    211012 삼항연산자

---
+++ js/threeway.js
@@ -0,0 +1,33 @@
+//삼항ㅇ연산자
+
+// const array = [];
+// let text='';
+// if(array.length === 0){
+//     text = '배열이 비어있습니다.';
+// } else {
+//     text = '배열이 비어있지 않습니다';
+// }
+
+// 위의 방법을 간단하게 작성할 수 있다.
+
+// let text =array.length === 0 
+//     ?'배열이 비어있습니다' // true
+//     : '배열이 비어있지 않습니다'; // false
+// console.log(text);
+
+
+
+// 또다른 경우
+
+// const condition1 = false;
+// const condition2 = false;
+
+// const value = condition1
+// ? '와우'
+// :condition2
+// ?'blabla'
+// :'foo'
+
+// console.log(value);
+
+// 삼항연산자를 중첩적으로 사용하지  않는 편이 좋다 복잡해진다.
