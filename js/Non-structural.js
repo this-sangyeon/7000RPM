dfdc0b27ca9bc324cf4ef3527ef219bbd41e1d9f
this-sangyeon <92290951+this-sangyeon@users.noreply.github.com>
2021-10-14 01:21:30 +0900

    211014 비구조할당

--- js/Non-structural.js
+++ js/Non-structural.js
@@ -45,10 +45,80 @@ const object = { a : 1 };
 // console.log(nickname);
 // console.log(animal);
 
-
+ 
 // ---------------------------------------------- !
 
 // 비구조할당은 배열에도 적용이 가능하다.
 
-const array = [ 1, 2];
-const one =  array[0];
+// const array = [ 1, 2];
+// // const one =  array[0];
+// // const two =  array[1];
+// // array에 담긴 값이 const 안의 배열에 적용된다.
+// const[one, two] = array;
+// // 기본값이 없는 경우 = 를 통해 할당해줄 수 있다.
+// // const[one, two = 2] = array;
+
+// console.log(one);
+// console.log(two);
+
+const deepObject ={
+    state : {
+        information :{
+            name: 'velopert',
+            languages : ['korean','engish','chinese']
+        }
+    },
+    value : 5
+}
+// 위의 식에서 만약 name, languages,  value을 식 바깥으로 빼고싶다면?
+// 두가지 방법이 있다.
+
+// const { name, languages } = deepObject.state.information;
+// const { value } = deepObject;
+// // 내가 꺼낸 것을 출력하고 싶다면.
+// // 특정키 이름으로 설정된 값이 있다면 value값 설정을 생략해도 된다.
+// const extracted = {
+//     name,
+//     languages,
+//     value
+// };
+// console.log(extracted);
+
+// 또 다른 방법
+// 비구조할당을 한번 실행하면서 값을 다 빼오는 방법
+
+// state 안에 있는 information 안에 있는 name, languages를 빼올 것이다.
+const  {
+    state : {
+        information : {
+            name, languages
+        }
+    },
+    value
+} = deepObject;
+
+const extracted = {
+    name,
+    languages,
+    value
+};
+console.log(extracted);
+
+// 위의 식의 다르게
+// 허나 지저분해보임.  되도록 위의 식으로
+// 상황에 따라 사용가능
+// const  {
+//     state : {
+//         information : {
+//             name, languages : [firstLang, secondLang]
+//         }
+//     },
+//     value
+// } = deepObject;
+
+// const extracted = {
+//     name,
+//     firstLang, secondLang,
+//     value
+// };
+// console.log(extracted);
