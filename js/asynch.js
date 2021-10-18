6fe71dd7c4d3e21f18374f708b9880ca77407d73
this-sangyeon <92290951+this-sangyeon@users.noreply.github.com>
2021-10-18 23:26:58 +0900

    비동기

--- js/asynch.js
+++ js/asynch.js
@@ -18,16 +18,26 @@
 // console.log('다음 작업');
 
 //비동기적
-function work(){
+function work(callback){
     // Data.now() js 내장함수 현재시각
-    const start = Date.now();
-    for(let i = 0; i < 1000000000; i++){
-
-    }
-    const end = Date.now();
-    // 얼마나 걸렸는지 알려주는 함수
-    console.log(end - start + 'ms');
+    setTimeout(() => {
+        const start = Date.now();
+        for(let i = 0; i < 1000000000; i++){
+    
+        }
+        const end = Date.now();
+        // 얼마나 걸렸는지 알려주는 함수
+        console.log(end - start + 'ms');
+        callback(end - start)
+    // 수치만큼의 시간이 지난 후 특정작업을 실행을 하겠다.
+    },0)
 }
 
-work();
+//callback 함수는
+//함수 타입ㅇ의값을 파라미터로 넘겨줘서 파라미터로 받은 값을 특정 작업이 끝나고 나서 호출하는 것
+console.log('작업시작')
+work((ms) =>{
+    console.log('작업종료');
+    console.log(ms+'ms 걸렸다고 해요.')
+});
 console.log('다음 작업');
