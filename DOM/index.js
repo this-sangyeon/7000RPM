b29d014bf6e1ef1989a7609cd34f8864d78d952d
this-sangyeon <92290951+this-sangyeon@users.noreply.github.com>
2021-10-22 19:46:25 +0900

    dom script

--- DOM/index.js
+++ DOM/index.js
@@ -66,4 +66,26 @@
 // ---------------------------------------!
 
 // 5
+//  class 추가, 삭제
+
+const link = document.querySelector('.testimg a');
+// 먼저 a에 걸려있던 class가 className을 추가하자 아래의 클래스로 교체되었다.
+// class 속성의 값을 바꿔버린다.
+// link.className ='special';
+// 두개를 다 같이 써주면 각각의 속성값이 적용된다. 하지만 비효율적
+// link.className ='special link-google';
+
+// 내가 넣고 싶은 속성을 추가하고 싶을 때
+// css의 class들에 관련된 기능을 가지고 있다.
+//classList.add() 클래스를 추가하겠다
+link.classList.add('special');
+//클래스를 여러개 추가하고 싶을 때 쉼표 하고 이어주면 된다.
+link.classList.add('special','foo');
+//classList.remove() 클래스를 삭제하겠다.
+link.classList.remove('link-google');
+// 2초 뒤에 함수를 실행하겠다.
+// 2초 뒹에 클래스를 삭제하겠다.
+setTimeout(()=>{
+    link.classList.remove('link-google');
+},2000);
 
