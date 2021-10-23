315548df796495dc4fa8676b9617d2f22f89c8ae
this-sangyeon <92290951+this-sangyeon@users.noreply.github.com>
2021-10-23 14:36:12 +0900

    dom event

---
+++ DOM2/script.js
@@ -0,0 +1,21 @@
+// console.log(window);
+
+// single element
+// const form = document.getElementById('todo-form');
+// console.log(document.getElementById('todo-form'))
+// const form = document.querySelector("#todo-form");
+// console.log(form);
+
+// multiple element
+// console.log(document.querySelectorAll('.item'));
+// const items = document.querySelectorAll('.item');
+// items.forEach((item)=>{
+//     console.log(item);
+// })
+// console.log(items);
+
+// console.log(document.getElementsByClassName('item'));
+// console.log(document.getElementsByTagName('li'));
+
+// 되도록 querySelector, querySelectorAll을 사용해주는 것이 효율적이다.
+
