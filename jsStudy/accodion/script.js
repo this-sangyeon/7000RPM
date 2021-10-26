076d81126eed8e4d21358fa1e22662578e536687
this-sangyeon <92290951+this-sangyeon@users.noreply.github.com>
2021-10-26 22:21:00 +0900

    accodion

---
+++ jsStudy/accodion/script.js
@@ -0,0 +1,3 @@
+const btnCollapse = document.querySelector('#btn-collapse');
+let headding = document.querySelectorAll('.panel-heading');
+const question = document.querySelector('.panel-question');
